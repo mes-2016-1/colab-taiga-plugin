6f2dac1659e4bf7f0443880514806fabbb8ed906
Gabriel Silva <gabriel93.silva@gmail.com>
2016-06-14 18:42:05 -0300

    Finishes project importing
    Signed-off-by: Gabriel Silva <gabriel93.silva@gmail.com>

--- src/colab_taiga/data_importer.py
+++ src/colab_taiga/data_importer.py
@@ -1,10 +1,13 @@
 import urllib2
 import json
+import logging
 
 from colab.plugins.data import PluginDataImporter
 
 from colab_taiga.models import TaigaProject
 
+
+LOGGER = logging.getLogger('colab_taiga')
 
 class TaigaPluginDataImporter(PluginDataImporter):
     app_label = 'colab_taiga'
@@ -15,10 +18,11 @@ class TaigaPluginDataImporter(PluginDataImporter):
 
     def get_data(self, url):
         try:
-            response = urllib2.urlopen(url, timeout=1000)
+            response = urllib2.urlopen(url, timeout=10)
             json_data = json.load(response)
             next_page = response.info().getheader('X-Pagination-Next')
         except urllib2.URLError:
+            LOGGER.exception('Failed to connect to Taiga API')
             json_data = {}
             next_page = None
 
@@ -30,15 +34,20 @@ class TaigaPluginDataImporter(PluginDataImporter):
         while next_page:
             json_data, next_page = self.get_data(next_page)
             for json_project in json_data:
-                project = TaigaProject()
+                project = TaigaProject.objects.get_or_create(
+                    id=json_project["id"])[0]
                 project.title = json_project['name']
                 project.description = json_project['description']
                 try:
                     project.save()
                 except:
+                    LOGGER.exception('Failed to import project with id=%s' %
+                                     json_project["id"])
                     continue
 
-
     def fetch_data(self):
+        LOGGER.info('Importing Taiga data')
+        LOGGER.info('Importing Projects...')
         self.fetch_projects()
 
+        LOGGER.info('Data imported')
