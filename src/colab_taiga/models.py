be482a895e5af2a1ae95ff5ee2d339d08c576cd0
caiquepereira <caiquepereira@gmail.com>
2016-06-30 19:13:10 -0300

    Importing taiga user stories
    Signed-off-by: caiquepereira <caiquepereira@gmail.com>
    Signed-off-by: Matheus S. Pereira <matheussilva.fga@gmail.com>

--- src/colab_taiga/models.py
+++ src/colab_taiga/models.py
@@ -16,3 +16,14 @@ class TaigaProject(models.Model):
     slug = models.TextField(blank=False, null=True)
     owner = models.ForeignKey(to=TaigaUser, related_name='own_projects')
     users = models.ManyToManyField(TaigaUser, related_name='projects')
+
+
+class TaigaUserStory(models.Model):
+    type = u'userstory'
+    subject = models.TextField(blank=True, null=True)
+    total_points = models.TextField(blank=True, null=True)
+    milestone = models.TextField(blank=True, null=True)
+    is_closed = models.BooleanField(default=False)
+    assigned_to = models.ForeignKey(to=TaigaUser, related_name='own_userstory',
+                                    null=True)
+    project = models.ForeignKey(TaigaProject, on_delete=models.CASCADE)
