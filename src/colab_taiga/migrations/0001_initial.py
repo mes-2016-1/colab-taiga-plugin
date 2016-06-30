f6d60abccdc9864fa772bc05771956db39e80784
Eduardo Moreira <moreira.edu@hotmail.com>
2016-06-30 18:56:24 -0300

    Creating only one migration to create the entire database
    Signed-off-by: Italo Paiva <italosensei@hotmail.com>

--- src/colab_taiga/migrations/0001_initial.py
+++ src/colab_taiga/migrations/0001_initial.py
@@ -13,13 +13,38 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='TaigaProject',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False,
-                 auto_created=True, primary_key=True)),
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('title', models.TextField()),
                 ('description', models.TextField(null=True, blank=True)),
+                ('slug', models.TextField(null=True)),
             ],
             options={
             },
             bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='TaigaUser',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('username', models.CharField(max_length=30)),
+                ('full_name', models.CharField(max_length=140)),
+                ('bio', models.TextField()),
+                ('is_active', models.BooleanField(default=True)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.AddField(
+            model_name='taigaproject',
+            name='owner',
+            field=models.ForeignKey(related_name='own_projects', to='colab_taiga.TaigaUser'),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='taigaproject',
+            name='users',
+            field=models.ManyToManyField(related_name='projects', to='colab_taiga.TaigaUser'),
+            preserve_default=True,
         ),
     ]
